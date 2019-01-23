c8d0ff9cd582184b21df8f01e8f315b60a0a902a
hanwei <hanwei@mafengwo.com>
2019-01-23 16:31:37 +0800

    提供Mq接口

---
+++ messageQueue/connection.go
@@ -0,0 +1,31 @@
+package messageQueue
+
+import (
+    "github.com/xxlixin1993/easyGo/logging"
+    "github.com/streadway/amqp"
+)
+
+type shareConn struct {
+    position    int8 //连接在池中的索引, 便于连接失败时，及时清除
+    conn        *amqp.Connection
+    maxReConn   int  //最大重连次数
+    isReachable bool // 连接是否可达
+}
+
+//共享连接是对底层amqp连接的封装, 记录连接在池中的位置，便于池中连接数的管理(剔除)
+func newShareConn(id int8, conn *amqp.Connection) *shareConn {
+    return &shareConn{position: id, conn: conn}
+}
+
+//获取连接
+func GetConnection() (*shareConn, error) {
+    if !rabbitPool.initialized {
+        err := rabbitPool.initialize()
+        if err != nil {
+            logging.FatalF("RabbitMq Connection Pool Initialize Failed! error: %v", err)
+        }
+    }
+    conn, index := rabbitPool.getConnection()
+
+    return newShareConn(index, conn), nil
+}
---
+++ messageQueue/consumer.go
@@ -0,0 +1,127 @@
+package messageQueue
+
+import (
+    "github.com/streadway/amqp"
+    "github.com/xxlixin1993/easyGo/configure"
+    "github.com/xxlixin1993/easyGo/logging"
+    "os"
+)
+
+/*var (
+    exchange     = configure.DefaultString("rabbitMq.exchange", "test-exchange")
+    exchangeType = configure.DefaultString("rabbitMq.exchangeType", "direct")
+    queueName    = configure.DefaultString("rabbitMq.queue", "test-queue")
+    bindingKey   = configure.DefaultString("rabbitMq.bindingKey", "test-key")
+)*/
+
+type consumerParam struct {
+    exchange string  //交换器名称
+    exchangeType string //交换器分发消息类型
+    queueName string  //队列名称
+    bindingKey string  // 交换器和队列的路由键
+    consumerTag string  //消费者的标识
+}
+
+func NewConsumer(consumerTag string) (*consumer, error) {
+    shareConn, err := GetConnection()
+    if err != nil {
+        logging.Warning("获取连接失败!", err)
+        return  nil, err
+    }
+
+    consumer := &consumer{conn: shareConn, tag: consumerTag}
+    return  consumer, nil
+}
+
+func (c *consumer)Consume(paramInfo consumerParam) (<-chan amqp.Delivery, error) {
+    var  err  error
+    c.channel, err = c.conn.Channel()
+
+    if err != nil {
+        logging.Warning("声明信道失败!", err)
+        return nil, err
+    }
+
+
+    if err = c.channel.ExchangeDeclare(
+        paramInfo.exchange,     //交换器名称
+        paramInfo.exchangeType, //交换器类型(fanout, direct, topic ,header)
+        true,         // 是否持久化
+        false,        // 是否自动删除交换器(前提是至少有一个交换器/队列与之相连接)
+        false,        //是否为内部使用，不对外
+        false,        // 是否等待服务端的确认
+        nil,          //额外参数
+    );err != nil {
+        logging.Warning("声明交换器失败!", err)
+        return nil, err
+    }
+
+    queue, err := c.channel.QueueDeclare(
+        paramInfo.queueName,    //队列名
+        true, // 是否持久化
+        false, // 是否自动删除(前提是至少有一个消费者与队列相连，若消费者断开，则会触发自动删除)
+        false,  //是否排他，(true:只有声明该队列的连接才能使用)
+        false,    //是否等待确认,(true:假定服务端已创建该队列)
+        nil,
+    )
+    if err != nil {
+        logging.Warning("声明队列失败!", err)
+        return nil, err
+    }
+
+    if err = c.channel.QueueBind(
+        queue.Name, // 队列名
+        paramInfo.bindingKey, // 绑定键
+        paramInfo.exchange,   // 交换器名
+        false,      // 不会等待服务端的确认
+        nil,         //额外参数
+    );err != nil {
+        logging.Warning("交换器队列绑定失败!", err)
+        return nil, err
+    }
+
+    deliveries, err := c.channel.Consume(
+        queue.Name,   // 队列名
+        c.tag,   //消费者的标识
+        false, //需要业务段处理完逻辑后，需要自行确认
+        false,  //是否排他, true:不会发给其他的消费者
+        false,   //The noLocal flag is not supported by RabbitMQ.
+        false,   // 是否等待服务端的确认
+        nil)       //额外参数
+
+    if err != nil {
+        logging.Warning("消费失败!", err)
+        return nil, err
+    }
+    return deliveries, nil
+}
+
+func loggingFatalF(format string, err error) {
+    if err != nil {
+        logging.FatalF(format, err)
+        os.Exit(configure.KInitGRPCCleintError)
+    }
+}
+
+type consumer struct {
+    conn       *shareConn
+    channel    *safeChannel
+    tag        string
+}
+
+func (c *consumer) Shutdown() error {
+
+    err := c.channel.originChannel.Cancel(c.tag, false)
+    if err != nil {
+        logging.FatalF("Channel 关闭失败!", err)
+    }
+
+    err = c.conn.conn.Close()
+    if err != nil {
+        logging.FatalF("连接 关闭失败!", err)
+        return err
+    }
+
+    logging.Info("消费者" + c.tag + "成功关闭!")
+    return nil
+}
